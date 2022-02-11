f2f2631c57f25459d04d92c64d6ad3a0eeb6f90c
saurabhchirde <saurabh123.chirde@gmail.com>
2022-02-11 22:04:48 +0530

    Landing page create, two more card shadow add

--- Documentation/docpage-js/docs.js
+++ Documentation/docpage-js/docs.js
@@ -1,11 +1,9 @@
 // hamburger menu
 document.querySelector(".hamburger").addEventListener("click", () => {
-    console.log("clicked ham");
     document.querySelector(".nav-mobile").style.display="flex";
 })
 
 document.querySelector(".nav-close-btn").addEventListener("click", () => {
-    console.log("clicked close")
     document.querySelector(".nav-mobile").style.display="none";
 })
 
